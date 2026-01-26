eca0f3b6ca96de84a407674daa1c828e2eed4232
Trương Minh Quý <truongminhquy46@gmail.com>
2026-01-27 02:59:03 +0700

    Update QUYSETUP.ps1

--- QUYSETUP.ps1
+++ QUYSETUP.ps1
@@ -28,25 +28,23 @@ Set-ItemProperty $reg DefaultDomainName $env:COMPUTERNAME
 Set-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System DisableCAD 1
 New-Item HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization -Force | Out-Null
 Set-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization NoLockScreen 1
- # ===============================
-        # STEP 2: DOWNLOAD QUYVM_USER.ps1
-        # ===============================
-        $userScript = "C:\QUYUSER.ps1"
 
-        Write-Host "[VM] Downloading QUYUSER.ps1 -> $userScript"
-
-        iwr -UseBasicParsing `
+# ===============================
+# STEP 2: DOWNLOAD QUYVM_USER.ps1
+# ===============================
+$userScript = "C:\QUYUSER.ps1"
+Write-Host "[VM] Downloading QUYUSER.ps1 -> $userScript"
+iwr -UseBasicParsing `
           https://raw.githubusercontent.com/IoT-VN/Hyper-V/refs/heads/main/QUYUSER.ps1 `
           -OutFile $userScript
-          icacls $userScript /grant "Users:R" /inheritance:e
-        # ===============================
-        # STEP 3: CREATE SCHEDULED TASK
-        # ===============================
-        Write-Host "[VM] Creating Scheduled Task for user liza"
-
-        $action = New-ScheduledTaskAction `
- -Execute "powershell.exe" `
- -Argument "-NoProfile -ExecutionPolicy Bypass -File `"$userScript`""
+icacls $userScript /grant "Users:R" /inheritance:e
+# ===============================
+# STEP 3: CREATE SCHEDULED TASK
+# ===============================
+Write-Host "[VM] Creating Scheduled Task for user liza"
+$action = New-ScheduledTaskAction `
+-Execute "powershell.exe" `
+-Argument "-NoProfile -ExecutionPolicy Bypass -File `"$userScript`""
 
 $trigger = New-ScheduledTaskTrigger -AtLogOn
 $trigger.Delay = "PT10S"   # rất quan trọng để Desktop load xong
@@ -63,8 +61,7 @@ Register-ScheduledTask `
  -Principal $principal `
  -Force
 
-
-        Write-Host ""
-        Write-Host "[VM] Setup complete"
-        Write-Host "[VM] User init will run at next logon (liza)"
+Write-Host ""
+Write-Host "[VM] Setup complete"
+Write-Host "[VM] User init will run at next logon (liza)"
 Restart-Computer -Force
