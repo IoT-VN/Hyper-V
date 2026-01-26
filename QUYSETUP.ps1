089e84e90d9acbf1414eb03132e208e433affc3a
Trương Minh Quý <truongminhquy46@gmail.com>
2026-01-27 02:53:16 +0700

    Update QUYSETUP.ps1

--- QUYSETUP.ps1
+++ QUYSETUP.ps1
@@ -28,5 +28,43 @@ Set-ItemProperty $reg DefaultDomainName $env:COMPUTERNAME
 Set-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System DisableCAD 1
 New-Item HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization -Force | Out-Null
 Set-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization NoLockScreen 1
+ # ===============================
+        # STEP 2: DOWNLOAD QUYVM_USER.ps1
+        # ===============================
+        $userScript = "C:\QUYUSER.ps1"
 
+        Write-Host "[VM] Downloading QUYUSER.ps1 -> $userScript"
+
+        iwr -UseBasicParsing `
+          https://raw.githubusercontent.com/IoT-VN/Hyper-V/refs/heads/main/QUYUSER.ps1 `
+          -OutFile $userScript
+          icacls $userScript /grant "Users:R" /inheritance:e
+        # ===============================
+        # STEP 3: CREATE SCHEDULED TASK
+        # ===============================
+        Write-Host "[VM] Creating Scheduled Task for user liza"
+
+        $action = New-ScheduledTaskAction `
+ -Execute "powershell.exe" `
+ -Argument "-NoProfile -ExecutionPolicy Bypass -File `"$userScript`""
+
+$trigger = New-ScheduledTaskTrigger -AtLogOn
+$trigger.Delay = "PT10S"   # rất quan trọng để Desktop load xong
+
+$principal = New-ScheduledTaskPrincipal `
+ -UserId "liza" `
+ -LogonType Interactive `
+ -RunLevel Highest
+
+Register-ScheduledTask `
+ -TaskName "QUYVM-UserInit" `
+ -Action $action `
+ -Trigger $trigger `
+ -Principal $principal `
+ -Force
+
+
+        Write-Host ""
+        Write-Host "[VM] Setup complete"
+        Write-Host "[VM] User init will run at next logon (liza)"
 Restart-Computer -Force
