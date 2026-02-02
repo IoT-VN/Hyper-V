e590454c8e5f86a591b0a9dc4741370c8efb9112
Trương Minh Quý <truongminhquy46@gmail.com>
2026-02-02 12:13:03 +0700

    Update index.ps1

--- index.ps1
+++ index.ps1
@@ -5,6 +5,8 @@ Write-Host "==============================="
 Write-Host ""
 Write-Host "1. VM"
 Write-Host "2. HOST"
+Write-Host "3.DOWNLOAD FILE STEP1"
+Write-Host "4.DOWNLOAD FILE STEP2"
 Write-Host ""
 $choice = Read-Host "Chon (1-2)"
 
@@ -28,6 +30,14 @@ switch ($choice) {
     default {
         Write-Host "[ERROR] Lua chon khong hop le" -ForegroundColor Red
     }
+     "3" {
+        Write-Host "[INFO] Loading DOWNLOAD FILE STEP1..."
+        iwr -UseBasicParsing https://raw.githubusercontent.com/IoT-VN/Hyper-V/refs/heads/main/ARIA2.ps1 | iex
+    }
+    "4" {
+        Write-Host "[INFO] Loading DOWNLOAD FILE STEP2..."
+        iwr -UseBasicParsing https://raw.githubusercontent.com/IoT-VN/Hyper-V/refs/heads/main/QUYHOST.ps1 | iex
+    }
 }
 
 Write-Host ""
@@ -35,6 +45,7 @@ Write-Host "==============================="
 Write-Host "            DONE               "
 
 Write-Host "==============================="
+
 
 
 
